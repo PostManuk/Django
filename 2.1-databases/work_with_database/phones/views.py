7a53d95831f1a44f19eaa34b1633b9e76f6111fd
PostManuk <118103143+PostManuk@users.noreply.github.com>
2024-02-04 21:01:20 +0300

    Update views.py

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Phone
 
 
 def index(request):
@@ -6,12 +7,19 @@ def index(request):
 
 
 def show_catalog(request):
+    sort = request.GET.get('sort', 'id')
+    if sort == 'min_price':
+        sort = 'price'
+    elif sort == 'max_price':
+        sort = '-price'
     template = 'catalog.html'
-    context = {}
+    phones = Phone.objects.all().order_by(sort)
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug__exact=slug)
+    context = {'phone': phone}
     return render(request, template, context)
